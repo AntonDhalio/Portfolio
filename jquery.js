fd37dda7b46e7a3917c6503b3c38019853ecc2c4
AntonDhalio <83759822+AntonDhalio@users.noreply.github.com>
2021-09-22 12:54:07 +0200

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $("#menu").hide();
     $(".staplar").hide();
+    $(".portfolioBild").hide();
+    $(".portfolioBild").slideDown(500);
+    $(".text").hide();
+    $(".text").delay(600).slideDown(800);
+    $(".visa-kunskap").hide();
+    $(".visa-kunskap").delay(1600).slideDown(500);
     $("#menu-icon").click(function(){
         $("#menu").show(400);
     });
