cb29992206afddd2787a0d8db53c36740d857772
madelenn <91068216+madelenn@users.noreply.github.com>
2021-09-23 10:29:04 +0200

    Försökt påbörja en startsida

--- js.js
+++ js.js
@@ -1,12 +1,125 @@
 $(document).ready(function(){
-    $('.länkBild').mouseenter(function(){
-      $(this).animate({
-          width: '30%'
-      });
+    // $('.länkBild').mouseenter(function(){
+    //   $(this).animate({
+    //       width: '30%'
+    //   });
+    // });
+    // $('.länkBild').mouseleave(function(){
+    //     $(this).animate({
+    //         width: '21%'
+    //     });
+    // });
+    $('#länkBild4').mouseenter(function(){
+        $(this).animate({
+            width: '30%'
+        });
+        $('#länkBild1').animate({
+            width: '16%'
+        });
+        $('#länkBild2').animate({
+            width: '16%' 
+        });
+        $('#länkBild3').animate({
+            width: '16%' 
+        });
+        
     });
-    $('.länkBild').mouseleave(function(){
+    $('#länkBild4').mouseleave(function(){
         $(this).animate({
             width: '21%'
+        });
+        $('#länkBild1').animate({
+            width: '21%'
+        });
+        $('#länkBild2').animate({
+            width: '21%' 
+        });
+        $('#länkBild3').animate({
+            width: '21%' 
+        });
+    });
+    $('#länkBild1').mouseenter(function(){
+        $(this).animate({
+            width: '30%'
+        });
+        $('#länkBild4').animate({
+            width: '16%'
+        });
+        $('#länkBild2').animate({
+            width: '16%' 
+        });
+        $('#länkBild3').animate({
+            width: '16%' 
+        });
+    });
+    $('#länkBild1').mouseleave(function(){
+        $(this).animate({
+            width: '21%'
+        });
+        $('#länkBild4').animate({
+            width: '21%'
+        });
+        $('#länkBild2').animate({
+            width: '21%' 
+        });
+        $('#länkBild3').animate({
+            width: '21%' 
+        });
+    });
+    $('#länkBild2').mouseenter(function(){
+        $(this).animate({
+            width: '30%'
+        });
+        $('#länkBild4').animate({
+            width: '16%'
+        });
+        $('#länkBild1').animate({
+            width: '16%' 
+        });
+        $('#länkBild3').animate({
+            width: '16%' 
+        });
+    });
+    $('#länkBild2').mouseleave(function(){
+        $(this).animate({
+            width: '21%'
+        });
+        $('#länkBild4').animate({
+            width: '21%'
+        });
+        $('#länkBild1').animate({
+            width: '21%' 
+        });
+        $('#länkBild3').animate({
+            width: '21%' 
+        });
+    });
+    $('#länkBild3').mouseenter(function(){
+        $(this).animate({
+            width: '30%'
+        });
+        $('#länkBild4').animate({
+            width: '16%'
+        });
+        $('#länkBild2').animate({
+            width: '16%' 
+        });
+        $('#länkBild1').animate({
+            width: '16%' 
+        });
+    });
+    $('#länkBild3').mouseleave(function(){
+        $(this).animate({
+            width: '21%'
+        });
+        $('#länkBild4').animate({
+            width: '21%'
+        });
+        $('#länkBild2').animate({
+            width: '21%' 
+        });
+        $('#länkBild1').animate({
+            width: '21%' 
         });
     });
 });
